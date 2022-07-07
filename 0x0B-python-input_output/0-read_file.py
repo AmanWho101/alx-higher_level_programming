d84cb11608dd897f84bb61381d156f9db6a7d632
AmanWho101 <Amleaz2005@gmail.com>
2022-07-07 17:59:41 +0300

    io

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Module 0-read_file.
+Reads from a file and prints.
+"""
+
+
 def read_file(filename=""):
-    """function that reads a text file and prints it"""
+    """Reads from filename and prints
+    its contents to stdout.
+    Args:
+        - filename: name of the file
+    """
 
     with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+        read_text = f.read()
+        print(read_text, end="")
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""
-===============================
-module with the class Student
-===============================
-"""
+'''Module for Student class.'''
 
 
 class Student:
-    """class with methods to_json for retrieves dictionary"""
-
+    '''Class for jsonification.'''
     def __init__(self, first_name, last_name, age):
-        """method for initialized all atributes"""
-
+        '''Constructor.'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        return self.__dict__
+    def to_json(self, attrs=None):
+        '''Retrieves dictionary with filter.'''
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        '''Loads attributes from json.'''
+        for key, value in json.items():
+            self.__dict__[key] = value
